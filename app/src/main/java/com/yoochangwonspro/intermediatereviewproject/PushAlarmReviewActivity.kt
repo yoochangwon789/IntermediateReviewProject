31dc1cb428a9a1f64a124f2b153e0800e2020f3a
CN <ckddnjs554@naver.com>
2021-08-13 16:27:50 +0900

    Feat : PushAlarmReviewActivity createFirebaseToken 메서드 구현

--- app/src/main/java/com/yoochangwonspro/intermediatereviewproject/PushAlarmReviewActivity.kt
+++ app/src/main/java/com/yoochangwonspro/intermediatereviewproject/PushAlarmReviewActivity.kt
@@ -3,6 +3,7 @@ package com.yoochangwonspro.intermediatereviewproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.firebase.messaging.FirebaseMessaging
 
 class PushAlarmReviewActivity : AppCompatActivity() {
 
@@ -17,9 +18,17 @@ class PushAlarmReviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_push_alarm_review)
+
+        createFirebaseToken()
     }
 
     private fun createFirebaseToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener {
+            if (!it.isSuccessful) {
+                return@addOnCompleteListener
+            }
 
+            tokenTextView.text = it.result
+        }
     }
 }
