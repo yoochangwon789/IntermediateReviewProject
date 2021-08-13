91c0a42c47af0c9da003564f81f80670468e22b1
CN <ckddnjs554@naver.com>
2021-08-13 16:12:15 +0900

    Feat : PushAlarmReviewActivity result 를 받는 TextView 레이아웃 구성

---
+++ app/src/main/java/com/yoochangwonspro/intermediatereviewproject/PushAlarmReviewActivity.kt
@@ -0,0 +1,11 @@
+package com.yoochangwonspro.intermediatereviewproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PushAlarmReviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_push_alarm_review)
+    }
+}
