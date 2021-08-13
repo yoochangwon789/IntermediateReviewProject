0eaa054c34c6f5daac75cf0cf056ba8b9c0ab495
CN <ckddnjs554@naver.com>
2021-08-13 16:20:46 +0900

    Feat : PushAlarmReviewActivity TextView property 설정

--- app/src/main/java/com/yoochangwonspro/intermediatereviewproject/PushAlarmReviewActivity.kt
+++ app/src/main/java/com/yoochangwonspro/intermediatereviewproject/PushAlarmReviewActivity.kt
@@ -2,10 +2,22 @@ package com.yoochangwonspro.intermediatereviewproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class PushAlarmReviewActivity : AppCompatActivity() {
+
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.push_alarm_result_text_view)
+    }
+
+    private val tokenTextView: TextView by lazy {
+        findViewById(R.id.push_alarm_result_token_text_view)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_push_alarm_review)
     }
+
+
 }
