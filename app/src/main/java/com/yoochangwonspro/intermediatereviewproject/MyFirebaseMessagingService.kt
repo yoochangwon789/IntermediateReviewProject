90ae3a1b9278d87e31d51945c9a7c6eaad7a639c
CN <ckddnjs554@naver.com>
2021-08-13 16:48:27 +0900

    Feat : PushAlarmReviewActivity MyFirebaseMessagingService class 추가 및 onNewToken 메서드 override 작업

---
+++ app/src/main/java/com/yoochangwonspro/intermediatereviewproject/MyFirebaseMessagingService.kt
@@ -0,0 +1,11 @@
+package com.yoochangwonspro.intermediatereviewproject
+
+import android.annotation.SuppressLint
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+    }
+}
