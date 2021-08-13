48d8c983286346fedbb7f1c590561abb59621c45
CN <ckddnjs554@naver.com>
2021-08-13 16:54:25 +0900

    Feat : PushAlarmReviewActivity MyFirebaseMessagingService class 의 onMessageReceived 메서드 override 작업

--- app/src/main/java/com/yoochangwonspro/intermediatereviewproject/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/yoochangwonspro/intermediatereviewproject/MyFirebaseMessagingService.kt
@@ -1,11 +1,15 @@
 package com.yoochangwonspro.intermediatereviewproject
 
-import android.annotation.SuppressLint
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(p0: String) {
         super.onNewToken(p0)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
     }
 }
