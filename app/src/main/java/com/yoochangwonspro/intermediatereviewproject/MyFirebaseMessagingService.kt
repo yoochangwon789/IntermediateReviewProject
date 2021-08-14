83c0cee620d53d92b4f432701f1add5a69aa7c25
CN <ckddnjs554@naver.com>
2021-08-14 15:43:22 +0900

    Feat : MyFirebaseMessagingService createNotificationChannel 채널 생성 함수 구현

--- app/src/main/java/com/yoochangwonspro/intermediatereviewproject/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/yoochangwonspro/intermediatereviewproject/MyFirebaseMessagingService.kt
@@ -1,5 +1,9 @@
 package com.yoochangwonspro.intermediatereviewproject
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -12,13 +16,30 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
 
+        createNotificationChannel()
+
         val type = remoteMessage.data["type"]
         val title = remoteMessage.data["title"]
         val message = remoteMessage.data["message"]
+
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = DESCRIPTION
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     companion object {
-        private const val CHANNEL_ID = "Chang Channel"
+        private const val CHANNEL_ID = "CHANG"
+        private const val CHANNEL_NAME = "Chang Channel"
         private const val DESCRIPTION = "Chang 채널 입니다."
     }
 }
